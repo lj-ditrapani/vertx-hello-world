1bad4de11a6e57a5d1a23eb4956a9d729cf266a8
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-06-01 16:02:58 -0400

    Give up on kts build
    Give up and use build.gradle from official jetbrains kotlin js tutorial

--- frontend/settings.gradle.kts
+++ frontend/settings.gradle.kts
@@ -1,11 +1,1 @@
 rootProject.name = "frontend"
-
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "kotlin2js") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-}

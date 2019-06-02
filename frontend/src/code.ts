d2faeda226c0aa02959e1aadec3377f63ee08ffe
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-06-02 18:22:37 -0400

    Remove extraneous console.logs

--- frontend/src/code.ts
+++ frontend/src/code.ts
@@ -4,11 +4,8 @@ $(document).ready(() => {
   console.log('hello world!')
   const button = $('#button')
   const counter = $('#counter')
-  console.log(button)
-  console.log(counter)
   button.click(() => $.get('/hello'))
   const location = document.location
-  console.log(location)
   const ws = new WebSocket(`ws://${location.hostname}:${location.port}`)
   ws.addEventListener('open', _ => {
     console.log('Connected')

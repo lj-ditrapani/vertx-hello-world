ca5b509530a3b852f734bc92f39fa9705468c05b
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-06-02 18:16:13 -0400

    Delete unused file

--- ws-test.js
+++
@@ -1,1 +0,0 @@
-var ws = new WebSocket("ws://localhost:44770"); ws.addEventListener('open', ev => { console.log('Connected') }); ws.addEventListener('message', ev => { console.log(`RECV: ${ev.data}`) });
